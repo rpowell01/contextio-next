e654886838c959890813d551320a92c0b563dbdb
Russell Powell <russell.powell@tierpoint.com>
2026-08-29 18:06:26 +0000

    lkj;oj

--- packages/web/components/ui/tooltip.tsx
+++ packages/web/components/ui/tooltip.tsx
@@ -102,18 +102,6 @@ function useTooltipContext() {
   return context;
 }
 
-function mergeRefs<T>(...refs: Array<React.Ref<T> | undefined | null>): React.RefCallback<T> {
-  return (value) => {
-    refs.forEach((ref) => {
-      if (typeof ref === "function") {
-        ref(value);
-      } else if (ref && typeof ref === "object") {
-        (ref as React.MutableRefObject<T | null>).current = value;
-      }
-    });
-  };
-}
-
 export function TooltipTrigger({ children, asChild = false }: TooltipTriggerProps) {
   const { showTooltip, hideTooltip, triggerRef, handleKeyDown, contentId } = useTooltipContext();
 
